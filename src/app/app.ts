ee6eb2142d112f9b784100ba37bb3ff574390f7f
Dinakrushna Mohanta <159710872+Dinakrushna7077@users.noreply.github.com>
2026-05-23 16:04:54 +0530

    Learning Events

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,99 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+//import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-ng-app');
+
+  userName:string="";
+  name:string="";
+  email="";
+  getUserName(event:Event)
+  {
+    console.log("Input value:",(event.target as HTMLInputElement).value);
+    this.name=(event.target as HTMLInputElement).value;
+  }
+  setUserName(event:Event)
+  {
+    this.userName=this.name;
+    console.log("UserName:",this.userName);
+  }
+
+  setDefaultName()
+  {
+    this.name="User";
+  }
+
+  getEmail(email:string)
+  {
+    console.log("Email:",email);
+  }
+
+  setEmail(email:string)
+  {
+    this.email=email;
+    console.log("Set Email:",this.email);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // evenHandler(event:MouseEvent)
+  // {
+  //   console.log("Button :",event.type);
+  // }
+  // inputEvent(event:Event)
+  // {
+  //   console.log("Input value:",(event.target as HTMLInputElement).value);
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // count=0;
+
+  // countHandler(type:string)
+  // {
+  //   if(type=='minus'&&this.count>0)
+  //   {
+  //     this.count--;
+  //   }
+  //   else if(type=='plus')
+  //   {
+  //     this.count++;
+  //   }
+  //   else
+  //   {
+  //     this.count=0;
+  //   }
+  // }
 }
